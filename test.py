88c92f3786b5a8491b178a64772088a50645b5d2
Maxime <maxime.mose.reg@gmail.com>
2022-06-18 19:18:49 +0300

    Add additional check

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from selene import have
+from selene import have, be
 from selene.support.shared import browser
 
 
 def test_search_git():
     browser.open('https://google.com')
-    browser.element('[name=q]').type('git').press_enter()
+    browser.element('[name=q]').should(be.blank).type('git').press_enter()
     browser.element('#search').should(have.text('https://git-scm.com'))
