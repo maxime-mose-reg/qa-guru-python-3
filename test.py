93636151918edcde18695d95777564b04d88d819
Maxime <maxime.mose.reg@gmail.com>
2022-06-18 19:29:06 +0300

    Modify test

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ from selene.support.shared import browser
 
 def test_search_git():
     browser.open('https://google.com')
-    browser.element('[name=q]').type('git').press_enter()
+    browser.element('[name=q]').set_value('git').press_enter()
     browser.element('#search').should(have.text('https://git-scm.com'))
